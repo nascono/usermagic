f7b4091f50d0a7012d1b80b32044ed115b844280
nascono <aryhuh@gmail.com>
2020-06-07 00:09:01 +0300

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -2,7 +2,7 @@
 if($_POST)
 {
 $db = mysqli_connect("eu-cdbr-west-03.cleardb.net","b622d4411afd95","fcdf179a","heroku_d686cc7dd5c25ab");
-$sql="INSERT INTO `form2` (`id`, `first_name`, `last_name`, `city`, `country`, `profession`, `age`, `gender`, `email`, `password`) VALUES (NULL, '".$_POST["first_name"]."', '".$_POST["last_name"]."', '".$_POST["city"]."', '".$_POST["country"]."', '".$_POST["profession"]."', '".$_POST["age"]."', '".$_POST["gender"]."', '".$_POST["gender"]."', '".$_POST["password"]."');";
+$sql="INSERT INTO `form2` (`id`, `first_name`, `last_name`, `city`, `country`, `profession`, `age`, `gender`, `email`, `password`) VALUES (NULL, '".$_POST["first_name"]."', '".$_POST["last_name"]."', '".$_POST["city"]."', '".$_POST["country"]."', '".$_POST["profession"]."', '".$_POST["age"]."', '".$_POST["gender"]."', '".$_POST["email"]."', '".$_POST["password"]."');";
 mysqli_query($db,"SET NAMES UTF8");
 mysqli_query($db,$sql);
 echo "Successfully register you are directing to main page";
