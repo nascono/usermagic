446c85b451d67cce7aa6e3893c8cf8f5a38857d6
nascono <aryhuh@gmail.com>
2020-06-16 01:34:28 +0300

    asd

--- sign_up.php
+++ sign_up.php
@@ -20,24 +20,7 @@ exit();
 </head>
 <body id="body">
 
-<div class="top">
-<div class="top_items">
-<div class="logo_box vertical-center">
-<img style="cursor: pointer; height:80px;" onclick="window.location='index.php'" src="res/logom.png"/>
-</div>
-
-
-<div class="other_buttons_box vertical-center">
-<button onclick="window.location='sign_up.php'" class="button_without_background">Get paid to test</button>
-<button onclick="window.location='solutions.php'" class="button_without_background">Solutions</button>
-<button onclick="window.location='index.php'" class="button_without_background">Pricing</button>
-<button onclick="window.location='index.php'" class="button_without_background">Contact us</button>
-<button onclick="window.location='try_for_free.php'" class="button_with_background">Try for free</button>
-<button onclick="window.location='sign_in.php'" class="button_without_background">Sign in</button>
-</div>
-
-</div>
-</div>
+<?php include("top.php"); ?>
 
 
 
