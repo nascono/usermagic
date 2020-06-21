07b2942c616635ef38df430d8d19e3fea78833b4
nascono <aryhuh@gmail.com>
2020-06-22 02:45:54 +0300

    Update login.php

--- routes/login.php
+++ routes/login.php
@@ -1,13 +1,13 @@
 <?php
 $out = array();
-if(isset($_POST["x_auth"]))
+if(isset($_GET["x_auth"]))
 {
-	if($_POST["x_auth"]="b9604d510ed0732b47b95e56392d0317")
+	if($_GET["x_auth"]="b9604d510ed0732b47b95e56392d0317")
 	{
 		$db = mysqli_connect("eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","nlc74woxcs5sif1d","mxbfj4mgfnaj3bi1","nb62b3bzhn3djx6q");
-		if(isset($_POST["email"])&&isset($_POST["password"]))
+		if(isset($_GET["email"])&&isset($_GET["password"]))
 		{
-			$sql ='SELECT * FROM `companies` WHERE `email` ="'.$_POST["email"].'" &&`password`="'.$_POST["password"].'"';
+			$sql ='SELECT * FROM `companies` WHERE `email` ="'.$_GET["email"].'" &&`password`="'.$_GET["password"].'"';
 			$result=mysqli_query($db,$sql);
 			$myvalue=mysqli_num_rows($result);
 			$rows= mysqli_fetch_array($result);
