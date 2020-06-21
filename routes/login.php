70b728b9ae9eff49069aa8033932e9b995af5e59
nascono <aryhuh@gmail.com>
2020-06-21 20:52:02 +0300

    asd

---
+++ routes/login.php
@@ -0,0 +1,34 @@
+<?php
+$out = array();
+if(isset($_POST["x_auth"]))
+{
+	if($_POST["x_auth"]="b9604d510ed0732b47b95e56392d0317")
+	{
+		$db = mysqli_connect("eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","nlc74woxcs5sif1d","mxbfj4mgfnaj3bi1","nb62b3bzhn3djx6q");
+		if(isset($_POST["email"])&&isset($_POST["password"]))
+		{
+			$sql ='SELECT * FROM `companies` WHERE `email` ="'.$_POST["email"].'" &&`password`="'.$_POST["password"].'"';
+			$result=mysqli_query($db,$sql);
+			$myvalue=mysqli_num_rows($result);
+			$rows= mysqli_fetch_array($result);
+			if($myvalue>0)
+			{
+				$out["id"]=$rows["id"];
+			}
+			else
+			{
+				$out["error_field"]="email or password is uncorrect";
+			}
+		}
+		else
+		{
+			$out["error_field"]="bad request";
+		}
+	}
+	else
+	{
+		$out["error_field"]="x_auth error";
+	}
+}
+print(json_encode($out));
+?>
