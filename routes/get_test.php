1851428b5e93c25f2513c87e0608b918ee44149a
nascono <aryhuh@gmail.com>
2020-06-22 19:18:48 +0300

    asd

---
+++ routes/get_test.php
@@ -0,0 +1,40 @@
+<?php
+$out = array();
+if(isset($_POST["x_auth"]))
+{
+	if($_POST["x_auth"]=="b9604d510ed0732b47b95e56392d0317")
+	{
+		$db = mysqli_connect("eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","nlc74woxcs5sif1d","mxbfj4mgfnaj3bi1","nb62b3bzhn3djx6q");
+		if(isset($_POST["campaign_id"]))
+		{
+			$sql = "SELECT * FROM `campaigns` WHERE `id` = ".$_POST["campaign_id"];
+			$result=mysqli_query($db,$sql);
+			$myvalue=mysqli_num_rows($result);
+			$rows= mysqli_fetch_array($result);
+			if($myvalue>0)
+			{
+				$out=json_decode($rows["test_json"]);
+			}
+			else
+			{
+				$out["error_field"]="id_is_invalid";
+			}
+		}
+		else
+		{
+			$out["error_field"]="bad_request";
+		}
+	}
+	else
+	{
+		$out["error_field"]="x_auth error";
+	}
+}
+else
+{
+	$out["error_field"]="bad_request";
+}
+print(json_encode($out));
+//echo "<pre>";
+//print_r(json_decode(json_encode($out)));
+?>
---
+++ routes/submit_test_result.php
@@ -0,0 +1,29 @@
+<?php
+$out = array();
+if(isset($_POST["x_auth"]))
+{
+	if($_POST["x_auth"]=="b9604d510ed0732b47b95e56392d0317")
+	{
+		$db = mysqli_connect("eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","nlc74woxcs5sif1d","mxbfj4mgfnaj3bi1","nb62b3bzhn3djx6q");
+		if(isset($_POST["user_id"])&&isset($_POST["campaign_id"])&&isset($_POST["test_answers"]))
+		{			
+			$sql="INSERT INTO `unapproved_test_answers` (`id`, `campaign_id`, `user_id`, `test_answers`) VALUES (NULL, '".$_POST["campaign_id"]."', '".$_POST["user_id"]."', '".$_POST["test_answers"]."')";
+			mysqli_query($db,$sql);
+			$out["unapproved_test_id"]=mysqli_insert_id($db);
+		}
+		else
+		{
+			$out["error_field"]="bad_request";
+		}
+	}
+	else
+	{
+		$out["error_field"]="x_auth_error";
+	}
+}
+else
+{
+	$out["error_field"]="bad_request";
+}
+print(json_encode($out));
+?>
